903a282496845332718a49265ce77575c6c96c98
Joao Camba <joaocamba@tests-iMac.local>
2023-05-26 14:09:42 +0100

    Expense Form skeleton, CSS improvements, ready for user input state additions

---
+++ jc-expense-tracking/src/components/NewExpense/NewExpense.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ExpenseForm from './ExpenseForm';
+import './NewExpense.css';
+
+const NewExpense = (props) => {
+
+  return (
+    <div>
+      <ExpenseForm />
+    </div>
+  )
+}
+
+export default NewExpense;
