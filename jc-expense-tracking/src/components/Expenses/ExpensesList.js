d23bba79f5dd7555c596eb2c7db33b191329c15e
Joao Camba <joaocamba@tests-iMac.local>
2023-05-26 21:51:51 +0100

    Chart bar additions

--- jc-expense-tracking/src/components/Expenses/ExpensesList.js
+++ jc-expense-tracking/src/components/Expenses/ExpensesList.js
@@ -4,21 +4,22 @@ import './ExpensesList.css';
 
 const ExpensesList = (props) => {
 
-  if (props.items.length() === 0) {
+  if (props.items.length === 0) {
     return (
       <h2 className="expenses-list__fallback">Found no expenses.</h2>
     )
   }
 
   return (
-    <ul className="expenses-list">
+    <ul className='expenses-list'>
       {props.items.map((expense) => (
         <ExpenseItem
           key={expense.id}
           title={expense.title}
           amount={expense.amount}
           date={expense.date}
-        />))}
+        />
+      ))}
     </ul>
   )
 }
